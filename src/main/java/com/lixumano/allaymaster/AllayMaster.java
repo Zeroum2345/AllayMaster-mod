01abcea0bc527ad31a22c3a32b0d5ac111194640
Zeroum2345 <marcuspsferreira@gmail.com>
2024-08-06 16:28:38 -0300

    white allay done

--- src/main/java/com/lixumano/allaymaster/AllayMaster.java
+++ src/main/java/com/lixumano/allaymaster/AllayMaster.java
@@ -1,5 +1,6 @@
 package com.lixumano.allaymaster;
 
+import com.lixumano.allaymaster.allays.WhiteAllay;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -23,15 +24,28 @@ public class AllayMaster implements ModInitializer {
 			EntityType.Builder.create(RedAllay::new, SpawnGroup.CREATURE).dimensions(0.75f, 0.75f).build()
 	);
 
+	public static final EntityType<WhiteAllay> WHITE_ALLAY = Registry.register(
+			Registries.ENTITY_TYPE,
+			Identifier.of("allaymaster", "white_allay"),
+			EntityType.Builder.create(WhiteAllay::new, SpawnGroup.CREATURE).dimensions(0.75f, 0.75f).build()
+	);
+
 	public static final Item RED_ALLAY_SPAWN_EGG = new SpawnEggItem(RED_ALLAY, 0xff1443, 0xff0000, new Item.Settings());
+	public static final Item WHITE_ALLAY_SPAWN_EGG = new SpawnEggItem(WHITE_ALLAY, 0xc4c4c4, 0xadadad, new Item.Settings());
 
 	@Override
 	public void onInitialize() {
 		FabricDefaultAttributeRegistry.register(RED_ALLAY, RedAllay.createAllayAttributes());
+		FabricDefaultAttributeRegistry.register(WHITE_ALLAY, WhiteAllay.createAllayAttributes());
 
 		Registry.register(Registries.ITEM, Identifier.of("allaymaster", "red_allay_spawn_egg"), RED_ALLAY_SPAWN_EGG);
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
 			content.add(RED_ALLAY_SPAWN_EGG);
+		});
+
+		Registry.register(Registries.ITEM, Identifier.of("allaymaster", "white_allay_spawn_egg"), WHITE_ALLAY_SPAWN_EGG);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> {
+			content.add(WHITE_ALLAY_SPAWN_EGG);
 		});
 	}
 }
